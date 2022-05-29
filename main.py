facc8373f645c5a42ec68269878b278223c234a6
Glohallo <gloriacarrascal53@gmail.com>
2022-05-28 23:20:38 -0500

    Update main.py

--- main.py
+++ main.py
@@ -31,19 +31,6 @@ async def root():
     return {"Message": "Hello World"}
 
 
-@app.get("/apod-planetary")
-async def planetary():
-    raw_response = requests.get(url)
-    response = json.loads(raw_response.text)
-    json_response = raw_response.json()
-    return json_response
-
-@app.get("/apod/count")
-async def planetary():
-    params = {'start_date': '2022-03-28', 'end_date':'2022-05-28'}
-    raw_response = requests.get(url, params=params)
-    json_response = raw_response.json()
-    return json_response
 {
   "json_response" : [...],
   "pagination": {
@@ -53,18 +40,36 @@ async def planetary():
   "count": "total number of items",
   "total": "total number of pages"
  }
+
 @app.get("/Query-pagination")
-async def planetary(page_num: int = 1, page_size: int = 10):
+async def astronomy(page_num: int = 1, page_size: int = 10):
     start = (page_num - 1)*page_size
     end = start + page_size
-    params = {'start_date': '2022-02-17', 'end_date':'2022-03-30'}
+    params = {'start_date': '2022-02-17', 'end_date':'2022-05-28', 'hd':True}
     raw_response = requests.get(url, params=params)
     json_response = raw_response.json()
+    # #converting to ditionary json_Dict
+    # json_Dict = json.loads(json_response)
+    list_response = []
+    for x in json_response:
+        print(x)
+        element = {}
+        if 'hdurl' in x:
+            element['hdurl'] = x['hdurl']
+        element['explanation'] = x['explanation']
+        element['title'] = x['title']
+        # element['hdurl'] = x['hdurl']
+        element['url'] = x['url']
+        list_response.append(element)
+        
+
+    json_response = list_response
     data_length = len(json_response)
     
+    #building pagination
     response = {
             "json_response": json_response[start:end],
-            "total": data_length,
+            "total": data_length - 1,
             "count": page_size,
             "pagination":{},
             }
@@ -85,24 +90,3 @@ async def planetary(page_num: int = 1, page_size: int = 10):
         return response
 
 
-
-# #connection
-# response = requests.get("https://api.nasa.gov/planetary/apod?api_key=0VdyL4qzq2P4OfTFJTV30pjKOOEj5HLjvf81QwBz")
-# #check if conection is ok
-# print(response.status_code)  #shows 200, all is ok 
-
-# #Part 2. Readding the data
-# print(response.json())
-# #creating a formatted string of the Python JSON object
-# def jprint(obj):
-#     text = json.dumps(obj, sort_keys=True, indent=4)
-#     print(text)
-        
-# print(jprint(response.json()))
-# #I have to call 110 data image
-# jprint(response.json()['date']['@attr'])
-
-# #asking by time image
-# range_date = pd.date_range(end = datetime.today(), periods = 100)
-# #convert pandas datetime to string column
-# range_string = range_date.strftime('%Y-%m-%d')
